83cb9e437af895d9e7890b5d3aef92ca7b0e6e8c
luca126 <lucadeutrich@gmail.com>
2023-07-12 14:24:07 +0200

    submodule files copied

---
+++ printerHousingCtrl/printerHousingCtrl/RGB_stm32/libs.h
@@ -0,0 +1,23 @@
+/**
+ *******************************************
+ * @file    libs.h
+ * @author  Dmitriy Semenov / Crazy_Geeks
+ * @brief   Internal header for adding sys libs and defines
+ *******************************************
+*/
+
+#ifndef LIBS_H_
+#define LIBS_H_
+
+#include "../../config.h"
+
+typedef uint8_t u8_t; 	///< 8-bit unsigned
+typedef int8_t i8_t;	///< 8-bit signed
+typedef uint16_t u16_t; ///< 16-bit unsigned
+typedef int16_t i16_t;	///< 16-bit signed
+typedef uint32_t u32_t; ///< 32-bit unsigned
+typedef int32_t i32_t;	///< 32-bit signed
+typedef float fl_t;	///< float type
+
+
+#endif /* LIBS_H_ */
