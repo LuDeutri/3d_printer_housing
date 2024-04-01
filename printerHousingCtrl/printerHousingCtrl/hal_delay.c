594fda4d9dbe7be6d6ee18ff4a19c1341bc7a40b
luca126 <lucadeutrich@gmail.com>
2024-04-01 20:05:29 +0200

    unused files deleted

--- printerHousingCtrl/printerHousingCtrl/hal_delay.c
+++
@@ -1,9 +0,0 @@
-#include "hal_delay.h"
-
-
-/*void delay_us (uint16_t us){
-	__HAL_TIM_SET_COUNTER(&TIM_HAL_DELAY,0);  // set the counter value a 0
-	while (__HAL_TIM_GET_COUNTER(&TIM_HAL_DELAY) < us);  // wait for the counter to reach the us input in the parameter
-}
-
-*/
