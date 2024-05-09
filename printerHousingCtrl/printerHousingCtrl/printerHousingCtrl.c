f77c7844ddd1467c3d7eda6310e7caf02a39e55b
luca126 <lucadeutrich@gmail.com>
2024-05-09 14:24:59 +0200

    errDelays for all pi inputs added top prevent bad connections errors, display screensaver input deleted

--- printerHousingCtrl/printerHousingCtrl/printerHousingCtrl.c
+++ printerHousingCtrl/printerHousingCtrl/printerHousingCtrl.c
@@ -1,26 +1,39 @@
 #include "printerHousingCtrl.h"
 
-bool printerSplyActive;
-bool raspiPrinterSplyInput;
+errDelay_t raspiLedInputOne;
+errDelay_t raspiLedInputTwo;
+errDelay_t raspiLedInputThree;
+errDelay_t raspiPrinterSplyCtrl;
+errDelay_t raspiScreenSaverCtrl;
+
+bool tmpDataInOne;
+bool tmpDataInTwo;
+bool tmpDataInThree;
 
 void printerHousingCtrl_init(){
-	// Init variables
-	printerSplyActive = true;
-	raspiPrinterSplyInput = true;
+
+	createErrDelay(&raspiLedInputOne, 1500);
+	createErrDelay(&raspiLedInputTwo, 1500);
+	createErrDelay(&raspiLedInputThree, 1500);
+	createErrDelay(&raspiPrinterSplyCtrl, 1500);
+	createErrDelay(&raspiScreenSaverCtrl, 1500);
+
+	tmpDataInOne = false;
+	tmpDataInTwo = false;
+	tmpDataInThree = false;
 
 	// Init modules
 	button_init();
 	fireExtinguisher_init();
 	gasSensor_init();
 	led_init();
-	screensaver_init();
 	printer_init();
 }
 
 void printerHousingCtrl_main(){
 
 	// Wait for finished init
-	if (HAL_GetTick() < 500)
+	if (HAL_GetTick() < 1000)
 		return;
 
 	// Main loop
@@ -33,7 +46,6 @@ void printerHousingCtrl_main(){
 		gasSensor_update();
 		fireExtinguisher_update();
 		led_update();
-		//screensaver_update();
 		printer_update();
 
 		// Toggle debug led
@@ -42,14 +54,21 @@ void printerHousingCtrl_main(){
 }
 
 void updatePiInput(){
-	// LED raspi pi inputs update
-	uint8_t tmpDataInOne = HAL_GPIO_ReadPin(pi_led_data_1_GPIO_Port, pi_led_data_1_Pin);
-	uint8_t tmpDataInTwo = HAL_GPIO_ReadPin(pi_led_data_2_GPIO_Port, pi_led_data_2_Pin);
-	uint8_t tmpDataInThree = HAL_GPIO_ReadPin(pi_led_data_3_GPIO_Port, pi_led_data_3_Pin);
+	if(HAL_GetTick() < 10000) // To ensure raspi has started completely
+		return;
 
+	// Check if the changed value is constant
+	if(checkErrDelay(&raspiLedInputOne, tmpDataInOne != HAL_GPIO_ReadPin(pi_led_data_1_GPIO_Port, pi_led_data_1_Pin)))
+		tmpDataInOne = HAL_GPIO_ReadPin(pi_led_data_1_GPIO_Port, pi_led_data_1_Pin);
+	if(checkErrDelay(&raspiLedInputTwo, tmpDataInTwo != HAL_GPIO_ReadPin(pi_led_data_2_GPIO_Port, pi_led_data_2_Pin)))
+		tmpDataInTwo = HAL_GPIO_ReadPin(pi_led_data_2_GPIO_Port, pi_led_data_2_Pin);
+	if(checkErrDelay(&raspiLedInputThree, tmpDataInThree != HAL_GPIO_ReadPin(pi_led_data_3_GPIO_Port, pi_led_data_3_Pin)))
+		tmpDataInThree = HAL_GPIO_ReadPin(pi_led_data_3_GPIO_Port, pi_led_data_3_Pin);
+
+	// LED raspi pi inputs update
 	leds.ledDataIn = (tmpDataInOne << 2) | (tmpDataInTwo << 1) | (tmpDataInThree);
 
 	// Printer supply input update
-	printer.splyCtrlRaspiInput = HAL_GPIO_ReadPin(pi_printer_sply_ctrl_GPIO_Port, pi_printer_sply_ctrl_Pin);
-	screensaver.raspiInput = HAL_GPIO_ReadPin(pi_displayScreensaver_GPIO_Port, pi_displayScreensaver_Pin);
+	if(checkErrDelay(&raspiPrinterSplyCtrl, printer.splyCtrlRaspiInput != HAL_GPIO_ReadPin(pi_printer_sply_ctrl_GPIO_Port, pi_printer_sply_ctrl_Pin)))
+		printer.splyCtrlRaspiInput = HAL_GPIO_ReadPin(pi_printer_sply_ctrl_GPIO_Port, pi_printer_sply_ctrl_Pin);
 }
