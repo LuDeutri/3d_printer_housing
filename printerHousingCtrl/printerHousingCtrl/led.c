57b9c3ffeeeb3b4a3f3f847fe438c4d8020557c1
luca126 <lucadeutrich@gmail.com>
2024-05-09 14:16:28 +0200

    runningLight start animation values added, reset brightness in nextState method added, fire detection led state changed, fire button led state changed, setAllLeds changed into setSPecificLeds for all states so that the electronic room leds are not controlled by them, timer for electronic room leds added (auto off after X seconds, white lightning)

--- printerHousingCtrl/printerHousingCtrl/led.c
+++ printerHousingCtrl/printerHousingCtrl/led.c
@@ -4,6 +4,12 @@ led_t leds;
 clock_t startTimeTimeExpired;
 
 bool startAnimation;
+uint32_t runningLightTime1;
+int32_t runningLightCounter1;
+uint32_t runningLightTime2;
+int32_t runningLightCounter2;
+
+clock_t startTimeLightElectronicsRoom;
 
 void led_init(){
 	leds.ledDataIn = 0;
@@ -14,97 +20,111 @@ void led_init(){
 	startAnimation = ENABLE_START_ANIMATION;
 
 	startTimeTimeExpired = 0;
+	runningLightTime1 = 0;
+	runningLightCounter1 = 0;
+
+	runningLightTime2 = 0;
+	runningLightCounter2 = 0;
+
+	startTimeLightElectronicsRoom = 0;
 
 	ws281x_init();
-
+	setBrightness(LED_BRIGHTNESS);
 }
 
 void led_update() {
 	// Start animation
-	if(startAnimation)
+	if(startAnimation){
 		startAnimationLED();
-
-	// Button ctrl, next state if button pressed
-	if(getButtonState(BUTTON_LED))
-		nextState();
-
-	// Update LEDs depending on raspi pi data input
-	if (leds.ledDataIn != leds.usedLedPiDataInput){
-		leds.usedLedPiDataInput = leds.ledDataIn; 	// Store the data in usedDataInput to be sure its only one time updated with the same data
-		nextState(leds.ledDataIn);
+		return;
 	}
 
-	// Overwrite led status if fire is detected
+	// Set the light in the electronics room off after LIGHT_TIME_ELECTRONICS_ROOM milliseconds
+	if(HAL_GetTick() > startTimeLightElectronicsRoom + LIGHT_TIME_ELECTRONICS_ROOM && startTimeLightElectronicsRoom != 0){
+		startTimeLightElectronicsRoom = 0;
+		setAllLEDs(off);
+	}
+
+	// Fire detected led mode
 	if(fireExtinguisher.activatingTime != 0)
 		nextStateLED(LED_STATE_RED_FADE);
-
-
-	//	TODO ----------- test circuit
-	leds.state = LED_STATE_RAINBOW;
-	//setBrightness(20);
-	// TODO --------------test circuit end
-
-	// Reset stored led data first
-	//setAllLEDs(off);
+	// Update LEDs depending on raspi pi data input
+	else if (leds.ledDataIn != leds.usedLedPiDataInput){
+		leds.usedLedPiDataInput = leds.ledDataIn; 	// Store the data in usedDataInput to be sure its only one time updated with the same data
+		nextStateLED(leds.ledDataIn);
+	// Button ctrl, next state if button pressed
+	} else if (getButtonState(BUTTON_LED)){
+		if(startTimeLightElectronicsRoom == 0){ // If the light in the electronics room is not active, activate it
+			if(leds.state == LED_STATE_OFF){
+				nextState();
+				return;
+			}
+			setSpecificLEDs(white, 0, NUM_LED_STRIP_BOTTOM_RIGHT);
+			setSpecificLEDs(white, INDEX_LED_STRIP_BOTTOM_RIGHT, NUM_LED);
+			startTimeLightElectronicsRoom = HAL_GetTick();
+		} else // If the light is already active, go into the next led state for the printer room leds
+			nextState();
+	}
 
 	switch(leds.state){
 	case LED_STATE_OFF:
 		setAllLEDs(off);
+		startTimeLightElectronicsRoom = 0;
 	break;
 	case LED_STATE_WHITE:
-		setAllLEDs(white);
+		setSpecificLEDs(white, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
 	case LED_STATE_GREEN:
-		setAllLEDs(green);
+		setSpecificLEDs(green, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
 	case LED_STATE_BLUE:
-		setAllLEDs(blue);
+		setSpecificLEDs(blue, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
 	case LED_STATE_RED:
-		setAllLEDs(red);
+		setSpecificLEDs(red, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
 	case LED_STATE_GREEN_FADE:
-		fadeOneColor(green, INDEX_LED_STRIP_LEFT, NUM_LED_PRINTER_ROOM);
+		fadeOneColor(green, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
 	case LED_STATE_RED_FADE:
-		fadeOneColor(red, INDEX_LED_STRIP_LEFT, NUM_LED_PRINTER_ROOM);
+		fadeOneColor(red, INDEX_LED_STRIP_LEFT, INDEX_LED_STRIP_BOTTOM_RIGHT);
 	break;
-	case LED_STATE_RAINBOW:
-		rainbow();
-	break;
-	// -------TODO test cycle ------
-	case LED_STATE_COLOR_BLINK:
-		blinkColors(colorBlinkReg, colorBlinkRegSize, 0, 5);
-		break;
 	case LED_STATE_COLOR_FADE:
-
-		cyclone();
-		//fadeColors(colorFadeReg, colorFadeRegSize, 0, 126);
-		break;
+		fadeColorWheel();
+	break;
 	default:
 		// You should never be here
 	break;
 	}
-	// Update led string
-	ws281x_send();
+	ws281x_send(); // Update led string
 }
 
 void startAnimationLED(){
 	// Set start animation to finished if one running light cylce reach the end
-	if(runningLight(red, 0, FORWARD, true) || runningLight(blue, NUM_LED, BACKWARD, true))
+	if(runningLight(red, 0, FORWARD, true, &runningLightTime1, &runningLightCounter1)
+			|| runningLight(blue, NUM_LED, BACKWARD, false, &runningLightTime2, &runningLightCounter2)) {
 		startAnimation = false;
+		setAllLEDs(off);
+	}
+	ws281x_send();
 }
 
 void nextStateLED(state_t nextStateLED) {
 	if (nextStateLED == leds.state)
 		return;
 
+	if(leds.state == LED_STATE_COLOR_FADE){
+		setAllLEDs(off);				 // Have to be done to reset the leds in the electronics room
+		setBrightness(LED_BRIGHTNESS);	// Reset the brightness
+	}
+
 	leds.state = nextStateLED;
 }
 
 void nextState() {
-	if(leds.state == LED_STATE_RAINBOW)
+	if(leds.state == LED_STATE_COLOR_FADE){
 		leds.state = LED_STATE_OFF;
-	else
+		setBrightness(LED_BRIGHTNESS);	// Reset the brightness
+	} else
 		leds.state++;
 }
