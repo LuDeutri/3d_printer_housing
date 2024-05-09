7fbc2f7cd3ded1d1b2a64619eb5fdc9c06b68c27
luca126 <lucadeutrich@gmail.com>
2024-05-09 14:23:30 +0200

    analog gas sensor value implemented, errDelay included

--- printerHousingCtrl/printerHousingCtrl/gasSensor.c
+++ printerHousingCtrl/printerHousingCtrl/gasSensor.c
@@ -14,21 +14,26 @@ void gasSensor_update(){
 	gasSensorReadData();
 
 	// Handle data
-	if (useAnalogGasValue){
-		// TODO Gas Sensor analog bewerten
-		// THRESHOLD_GAS_SENSOR_FIRE_DETECTED
-	} else if (gasSensor.gasSensDig)
-		gasSensor.fireDetected = true;
-
-	// Activate fire extinguisher after a defined error delay
-	if(gasSensor.fireDetected && gasSensor.fireDetectedTime + ERR_DELAY_FIRE_DETECTED > HAL_GetTick())
-		fireExtinguisherActivate();
+	if ((gasSensor.gasSensDig && !ENABLE_ANALOG_GAS_VALUE) 			// Fire detected by the sensor
+			|| (gasSensor.gasSensAnalog > THRESHOLD_GAS_SENSOR_FIRE_DETECTED && ENABLE_ANALOG_GAS_VALUE)) {
+		if(gasSensor.fireDetectedTime == 0) // Safe time fire is detected
+			gasSensor.fireDetectedTime = HAL_GetTick();
+		else if(gasSensor.fireDetectedTime + ERR_DELAY_FIRE_DETECTED < HAL_GetTick()){ // Wait err_delay
+			gasSensor.fireDetected = true;
+			fireExtinguisherStartCount();			// Activate the fire extinguisher sequence
+		}
+	}
 }
 
 void gasSensorReadData(){
+	if(HAL_GetTick() < 30000) // Wait 10 seconds to calibrate the sensor first
+		return;
+
 	gasSensor.gasSensDig = HAL_GPIO_ReadPin(gas_sens_digital_GPIO_Port, gas_sens_digital_Pin);
 
-	if (useAnalogGasValue){
-		// TODO Read out analog data of gas sensor
+	if (ENABLE_ANALOG_GAS_VALUE){
+		HAL_ADC_Start(&hadc1);
+		HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+		gasSensor.gasSensAnalog = HAL_ADC_GetValue(&hadc1);
 	}
 }
