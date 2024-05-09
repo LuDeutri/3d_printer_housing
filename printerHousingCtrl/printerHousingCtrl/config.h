56ae718a9390ec7612ded018c4404957867e2187
luca126 <lucadeutrich@gmail.com>
2024-05-09 14:19:17 +0200

    errDelay include added, timer time for light in electronics room added, some parameters changed, display screensaver defines deleted, gasSensor boolean changed into define

--- printerHousingCtrl/printerHousingCtrl/config.h
+++ printerHousingCtrl/printerHousingCtrl/config.h
@@ -11,6 +11,8 @@
 #include <time.h>
 #include <stddef.h>
 
+#include "../utility/errDelay.h"
+
 #include"../Core/Inc/main.h"
 #include <stm32f1xx_hal.h>
 
@@ -30,14 +32,15 @@
 // LED strips
 // #define NUM_LED defined in ws281.h
 #define ENABLE_START_ANIMATION 1		// 0: start animation is disabled
-#define LED_BRIGHTNESS 45				// 0 ... 45
+#define LED_BRIGHTNESS 30				// 0 ... 45
 #define TIME_STARTING_ANIMATION 3000	// max duration of the start animation
 
 // Electronics room
 #define INDEX_LED_STRIP_BOTTOM_LEFT 0	// Index of the first led of this strip
 #define NUM_LED_STRIP_BOTTOM_LEFT 15 	// In the electronics area under the printer left side
+#define LIGHT_TIME_ELECTRONICS_ROOM 20000	// Duration how long the light in the electronics room is active after pressing the light button
 
-#define INDEX_LED_STRIP_BOTTOM_RIGHT 106// Index of the first led of this strip
+#define INDEX_LED_STRIP_BOTTOM_RIGHT 109 // Index of the first led of this strip
 #define NUM_LED_STRIP_BOTTOM_RIGHT 15	// In the electronics area under the printer right side
 
 // Printer room led 14 - 105
@@ -48,23 +51,22 @@
 #define NUM_LED_STRIP_TOP 39			// About the printer
 
 #define INDEX_LED_STRIP_RIGHT 90		// Index of the first led of this strip
-#define NUM_LED_STRIP_RIGHT 16			// Right to the printer
+#define NUM_LED_STRIP_RIGHT 15			// Right to the printer
 
 #define NUM_LED_PRINTER_ROOM NUM_LED_STRIP_LEFT+NUM_LED_STRIP_TOP+NUM_LED_STRIP_RIGHT
 
 // Fire extinguisher
 #define FIRE_EXTINGUISHER_TRIGGER_TIMER 20000 		// Time until co2 valve is opening
-#define FIRE_EXTINGUISHER_EXIT_BTN_PRESS_TIME 2000	// If the Btn is pressed over that limit during fire extinguisher is active, extinguisher will be stopped
+#define FIRE_EXTINGUISHER_EXIT_BTN_PRESS_TIME 1	// If the Btn is pressed over that limit during fire extinguisher is active, extinguisher will be stopped
 #define ERR_DELAY_FIRE_DETECTED 2000
 #define THRESHOLD_GAS_SENSOR_FIRE_DETECTED 100 		// TODO define fire detected threshold for analog value
 
 // Beeper
-#define TIME_REDUCE_BEEPER 300000 				// beeper will be reduced
+#define TIME_REDUCE_BEEPER 120000 				// beeper will be reduced
 
-// Display
-#define RESET_RASPI_DISPLAYSCREENSAVER_INPUT_IGNORE 3000 // After this time, the pi can ctrl the screensaver again
-
-extern const bool useAnalogGasValue;
+// Gas sensor
+extern ADC_HandleTypeDef hadc1;
+#define ENABLE_ANALOG_GAS_VALUE 1 // 0: digital value is used, 1: analog value is used
 
 // ########################################
 // Software configuration end
